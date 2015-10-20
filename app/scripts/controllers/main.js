9b1ba6c6edcf9e7ba14138900223abcea832e3a7
Arturs Vancans <connect@artursvancans.com>
2015-10-20 01:31:05 +0100

    First draft

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,46 @@
 'use strict';
 
 
-angular.module('RPSApp').controller('MainCtrl', ['$scope',  function ($scope) {
+angular.module('RPSApp').controller('MainCtrl', ['$scope', 'RPSService',  function ($scope,  RPSService) {
+
+    $scope.inGame = false;
+    $scope.humanMode = false;
+    $scope.choices = RPSService.getChoices();
+
+    $scope.result = function() {
+        return RPSService.result();
+    };
+
+    $scope.selectedChoice = function() {
+        return RPSService.selectedChoice();
+    };
+
+    $scope.computerChoice = function() {
+        return RPSService.computerChoice();
+    };
+
+
+    $scope.playComputer = function() {
+        $scope.inGame = true;
+        $scope.humanMode = false;
+        RPSService.playComputer();
+
+    };
+
+    $scope.playHuman = function() {
+        $scope.inGame = true;
+        $scope.humanMode = true;
+    };
+
+    $scope.userPick = function(choice) {
+        RPSService.playHuman(choice);
+    };
+
+    $scope.resetGame = function() {
+        $scope.inGame = false;
+        $scope.humanMode = false;
+        RPSService.resetGame();
+    };
 
 
 }]);
--- app/scripts/services/rps_service.js
+++ app/scripts/services/rps_service.js
@@ -1,7 +1,88 @@
 'use strict';
 
 
-angular.module('RPSApp').factory('RPS_Service', [ function () {
+angular.module('RPSApp').factory('RPSService', ['$timeout', function ($timeout) {
+
+    var ROCK = 'ROCK';
+    var PAPER = 'PAPER';
+    var SCISSORS = 'SCISSORS';
+
+    var DRAW = 'DRAW';
+    var VICTORY = 'VICTORY';
+    var DEFEAT = 'DEFEAT';
+
+    var result = false;
+    var selectedChoice = null;
+    var computerChoice = null;
+
+
+    var choices = [ROCK, PAPER, SCISSORS];
+
+    var lookupTable = {};
+
+    choices.forEach(function(choice, i) {
+        lookupTable[choice] = {};
+        lookupTable[choice][choice] = DRAW;
+        lookupTable[choice][choices[(i+1)%3]] = VICTORY;
+        lookupTable[choice][choices[(i+2)%3]] = DEFEAT;
+    });
+
+    function playComputer() {
+
+        $timeout(function() {
+            selectedChoice = choices[Math.floor(Math.random() * 3)];
+            $timeout(function() {
+                computerChoice = choices[Math.floor(Math.random() * 3)];
+                $timeout(function() {
+                    result  = lookupTable[computerChoice][selectedChoice];
+                }, 1500);
+            }, 2000);
+        }, 2000);
+
+    }
+
+    function playHuman(choice) {
+
+        selectedChoice = choice;
+
+        console.log(choice);
+
+        $timeout(function() {
+            computerChoice = choices[Math.floor(Math.random() * 3)];
+            $timeout(function() {
+                result  = lookupTable[computerChoice][selectedChoice];
+            }, 1500);
+
+        }, 2000);
+
+    }
+
+
+    function resetGame() {
+        result = false;
+        selectedChoice = null;
+        computerChoice = null;
+    }
+
+
+
+    return  {
+        getChoices: function() {
+            return choices;
+        },
+        playComputer: playComputer,
+        playHuman: playHuman,
+        resetGame: resetGame,
+        result: function() {
+            return result;
+        },
+        selectedChoice: function() {
+            return selectedChoice;
+        },
+        computerChoice: function() {
+            return computerChoice;
+        }
+    };
 
 }]);
 
